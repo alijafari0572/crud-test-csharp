bc41eab2c6d1597eaf369a23f81a909d9c849ee0
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:32 +0330

    Create ICheckBankAccountNumber.cs

---
+++ WebApi/Repositories/ICheckBankAccountNumber.cs
@@ -0,0 +1,9 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public interface ICheckBankAccountNumber
+    {
+        public bool CheckValidBankAccountNumber(Customer_Model customerModel);
+    }
+}
