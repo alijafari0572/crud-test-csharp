2772141af7188f117e6ccaf6dbcdec798a252fa0
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:42 +0330

    Create CheckEmail.cs

---
+++ WebApi/Repositories/CheckEmail.cs
@@ -0,0 +1,14 @@
+﻿using EmailValidation;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class CheckEmail:ICheckEmail
+    {
+        public bool CheckValidEmail(Customer_Model customerModel)
+        {
+            var IsValidEmail = EmailValidator.Validate(customerModel.Email);
+            return IsValidEmail;
+        }
+    }
+}
