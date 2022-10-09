234cfa34c281bc3bfc9cb190fe52d212e0d882ef
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:45 +0330

    Create CheckBankAccountNumber.cs

---
+++ WebApi/Repositories/CheckBankAccountNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class CheckBankAccountNumber:ICheckBankAccountNumber
+    {
+        public bool CheckValidBankAccountNumber(Customer_Model customerModel)
+        {
+            string regex = @"^[0-9]{16,20}$";
+            var IsValidBankAccount = Regex.IsMatch(customerModel.BankAccountNumber, regex);
+            return IsValidBankAccount;
+
+        }
+    }
+}
