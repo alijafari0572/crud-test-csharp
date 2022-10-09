5c44d267b9628a2bddaddd7712df8b2675602993
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:35 +0330

    Create CheckUnique.cs

---
+++ WebApi/Repositories/CheckUnique.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class CheckUnique:ICheckUnique
+    {
+        private readonly WebApiDBContext _context;
+
+        public CheckUnique(WebApiDBContext context)
+        {
+            _context = context;
+        }
+
+        public bool checkunique(Customer_Model customerModel)
+        {
+            var firstname = customerModel.FirstName;
+            var lastname = customerModel.LastName;
+            var dob = customerModel.DoB;
+            var email = customerModel.Email;
+            if(_context.Customers.Any(a=>a.FirstName==firstname))
+                return true;
+            if (_context.Customers.Any(a => a.LastName == lastname))
+                return true;
+            if (_context.Customers.Any(a => a.DoB == dob))
+                return true;
+            if (_context.Customers.Any(a => a.Email == email))
+                return true;
+            return false;
+        }
+    }
+}
