5258eda78051cc35207748f20f19a778ec9364b4
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:15:17 +0330

    Create Customer.cs

---
+++ Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    [Index(nameof(FirstName), IsUnique = true)]
+    [Index(nameof(LastName), IsUnique = true)]
+    [Index(nameof(DoB), IsUnique = true)]
+    [Index(nameof(Email), IsUnique = true)]
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
+    }
+}
