5f8c37a50ab9899e6290a60f5a0aa90bcb52150d
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:39 +0330

    Create CheckPhonenumber.cs

---
+++ WebApi/Repositories/CheckPhonenumber.cs
@@ -0,0 +1,16 @@
+﻿
+using PhoneNumbers;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class CheckPhonenumber: ICheckPhonenumber
+    {
+        public bool CheckValidPhoneNumber(Customer_Model customerModel)
+        {
+            var IsViablePhoneNumber = PhoneNumberUtil.IsViablePhoneNumber(customerModel.PhoneNumber);
+
+            return IsViablePhoneNumber;
+        }
+    }
+}
