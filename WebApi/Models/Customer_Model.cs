e539291edc00d24a73cd1d185b8411289f0b83ea
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:56 +0330

    Create Customer_Model.cs

---
+++ WebApi/Models/Customer_Model.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Models
+{
+    public class Customer_Model
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
+    }
+}
