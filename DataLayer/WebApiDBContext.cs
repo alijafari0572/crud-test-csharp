112c1ef65d42b45798804d861b2ddbc502d16070
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:15:19 +0330

    Create WebApiDBContext.cs

---
+++ DataLayer/WebApiDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+namespace DataLayer
+{
+    public class WebApiDBContext : DbContext
+    {
+        public WebApiDBContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
