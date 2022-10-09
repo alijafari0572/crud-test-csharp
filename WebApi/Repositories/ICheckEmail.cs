845e6d4c77c89ac30686b3125087bcfa62f2f38a
alijafari0572 <alijafari0572@gmail.com>
2022-10-10 01:14:27 +0330

    Create ICheckEmail.cs

---
+++ WebApi/Repositories/ICheckEmail.cs
@@ -0,0 +1,10 @@
+﻿using WebApi.Models;
+using System.Threading.Tasks;
+namespace WebApi.Repositories
+{
+    public interface ICheckEmail
+    {
+        public bool CheckValidEmail (Customer_Model customerModel);
+
+    }
+}
